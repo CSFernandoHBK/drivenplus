08a4d85521f27d3ba6f1c1d7f53971788895e798
Fernando <fernandotam2@hotmail.com>
2022-10-27 00:28:46 -0300

    home static version

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,24 @@
 import styled from "styled-components";
+import { Icon } from '@iconify/react';
+import logoPlano1 from "../../assets/images/logoPlano1.png"
 
 export default function HomePage() {
-    <Container>
-        <img/>
-        <img/>
-        <div>
-            <p>Olá, fulano</p>
-            <button>Solicitar brindes</button>
-            <button>Materiais bônus de web</button>
-        </div>
-    </Container>
+    return (
+        <Container>
+            <img src={logoPlano1} />
+            <Icon icon="fa-solid:user-circle" />
+            <div>
+                <p>Olá, fulano</p>
+                <Botao>Solicitar brindes</Botao>
+                <Botao>Materiais bônus de web</Botao>
+            </div>
+            <div>
+                <BotaoMudarPlano>Mudar plano</BotaoMudarPlano>
+                <BotaoCancelarPlano>Cancelar plano</BotaoCancelarPlano>
+            </div>
+        </Container>
+    )
+
 }
 
 const Container = styled.div`
@@ -18,6 +27,62 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     background-color: #0E0E13;
+    position: relative;
+
+    p{
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 28px;
+        color: #FFFFFF;
+        margin-bottom: 45px;
+    }
+    img{
+        height: 51px;
+        position: absolute;
+        top: 32px;
+        left: 38px;
+    }
+    svg{
+        color: #FFFFFF;
+        font-size: 34px;
+        position: absolute;
+        top: 22px;
+        right: 22px;
+    }
+
+    & > div:nth-child(3){
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 95px;
+    }
+
+    & > div:nth-child(4){
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 12px;
+    }
+`
+
+const Botao = styled.button`
+    background: #FF4791;
+    border-radius: 8px;
+    width: 299px;
+    height: 52px;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    color: #FFFFFF;
+    margin-top: 8px;
+`
+
+
+const BotaoMudarPlano = styled(Botao)`
+    background: #FF4791;
+`
+const BotaoCancelarPlano = styled(Botao)`
+    background: #FF4747;
 `
